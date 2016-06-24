b05f13a5117908d61ec09f2390be3111b2eea962
Christian Kovar <ckovar82@gmail.com>
2016-06-24 16:36:21 +0200

    Initial Upload CrossOver Condition
    Initial Upload CrossOver Condition

---
+++ Example_Indicator_Condition_Strategy_Basic/ScriptedConditions/Example_Condition_SMA_CrossOver_Basic.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using AgenaTrader.API;
+using AgenaTrader.Custom;
+using AgenaTrader.Plugins;
+using AgenaTrader.Helper;
+
+namespace AgenaTrader.UserCode
+{
+	[Description("Basic condition example for SMA crossover")]
+	[IsEntryAttribute(true)]
+	[IsStopAttribute(false)]
+	[IsTargetAttribute(false)]
+	[OverrulePreviousStopPrice(false)]
+	public class Example_Condition_SMA_CrossOver_Basic : UserScriptedCondition
+	{
+		protected override void Initialize()
+		{
+			IsEntry = true;
+			IsStop = false;
+			IsTarget = false;
+			Add(new Plot(Color.FromKnownColor(KnownColor.Black), "Occurred"));
+			Add(new Plot(Color.FromArgb(255, 141, 115, 149), "Entry"));
+			Overlay = true;
+			CalculateOnBarClose = true;
+		}
+
+		protected override void OnBarUpdate()
+		{
+
+            //get the indicator
+            Example_Indicator_SMA_CrossOver_Basic Example_Indicator_SMA_CrossOver_Basic = LeadIndicator.Example_Indicator_SMA_CrossOver_Basic();
+
+            //get the value
+            double returnvalue = Example_Indicator_SMA_CrossOver_Basic[0];
+
+            //set the value
+            Occurred.Set(returnvalue);
+		}
+
+        //defines display name of indicator (in AgenaTrader indicator selection window)
+        public override string DisplayName
+        {
+            get
+            {
+                return "Example SMA CrossOver Basic";
+            }
+        }
+
+		#region Properties
+
+		[Browsable(false)]
+		[XmlIgnore()]
+		public DataSeries Occurred
+		{
+			get { return Values[0]; }
+		}
+
+		[Browsable(false)]
+		[XmlIgnore()]
+		public DataSeries Entry
+		{
+			get { return Values[1]; }
+		}
+
+		public override IList<DataSeries> GetEntries()
+		{
+			return new[]{Entry};
+		}
+
+		#endregion
+	}
+}
